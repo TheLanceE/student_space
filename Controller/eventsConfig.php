cbbbbbd2459f26a85a28a6f8a4c0b08824e528c5
Weblynx Dev <contact@weblinx.studio>
2025-11-18 11:43:57 +0100

    refactor: Remove duplicate old MVC folders from Events branch
    - Deleted Controller/, Model/, View/ (old Events branch structure)
    - Keeping Controllers/, Models/ (new standardized MVC structure)
    - All Events functionality now in teacher-back-office/events.php and admin-back-office/events.php

--- Controller/eventsConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $serverName = "localhost";
-    $userName = "root";
-    $password = "";
-    $dataBase = "Events";
-
-    $pdo = new PDO("mysql:host=$serverName;dbname=$dataBase", $userName, $password);
-?>
